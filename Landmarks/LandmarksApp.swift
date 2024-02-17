1b6ff619603cc3807142733e9c0246ec10995d68
Christian Ferrer <cferrer@pibox.app>
2024-02-16 22:06:34 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Christian Alexis Ferrer Cava on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
